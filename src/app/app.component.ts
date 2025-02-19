8016f674ac3141131142611a6551eb11ccd111da
cookiegpjr <cookiegpjr@gmail.com>
2025-02-19 10:57:28 +0000

    Front Login Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [LoginComponent],
+  template: `<app-login></app-login>`,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'alex-claros-LogInA';
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  onSubmit() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
+}
+
